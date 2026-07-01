af2b4fe08bce39bc6658c79efe9fe017db335e86
top-secret666 <chupacabra2.0666@gmail.com>
2026-07-01 14:28:31 +0300

    build: simplify run.bat to use compile-swing.bat for building

---
+++ compile-swing.bat
@@ -0,0 +1,38 @@
+@echo off
+setlocal EnableDelayedExpansion
+cd /d "%~dp0"
+
+set "JAVA_HOME=C:\Program Files\Java\jdk-17"
+set "SRC=%~dp0src\main\java\com\witcher"
+set "RES=%~dp0src\main\resources"
+set "OUT=%~dp0out\swing-run"
+set "TMP=%~dp0.tmp"
+if not exist "%TMP%" mkdir "%TMP%"
+
+if not exist "%JAVA_HOME%\bin\javac.exe" (
+  echo [ОШИБКА] JDK 17 не найден: %JAVA_HOME%
+  echo Укажи верный JAVA_HOME в compile-swing.bat
+  pause
+  exit /b 1
+)
+
+if not exist "%OUT%" mkdir "%OUT%"
+
+echo === Компиляция Swing (все .java из src\main\java\com\witcher) ===
+dir /s /b "%SRC%\*.java" > "%TMP%\swing-sources.txt"
+"%JAVA_HOME%\bin\javac.exe" -encoding UTF-8 -d "%OUT%" @"%TMP%\swing-sources.txt"
+if errorlevel 1 (
+  echo.
+  echo [ОШИБКА] Компиляция не удалась. Исправь ошибки выше.
+  pause
+  exit /b 1
+)
+
+if exist "%RES%" (
+  echo === Копирование ресурсов ===
+  powershell -NoProfile -ExecutionPolicy Bypass -Command "Copy-Item -LiteralPath '%RES%\*' -Destination '%OUT%' -Recurse -Force"
+)
+
+echo.
+echo Сборка OK: %OUT%
+exit /b 0
--- run.bat
+++ run.bat
@@ -3,27 +3,18 @@ setlocal
 cd /d "%~dp0"
 
 set "JAVA_HOME=C:\Program Files\Java\jdk-17"
-set "RES=%~dp0src\main\resources"
-set "BIN="
+set "OUT=%~dp0out\swing-run"
 
-if exist "%~dp0out\production\the-witcher\main\java\com\witcher\ui\graphics\GameWindow.class" (
-  set "BIN=%~dp0out\production\the-witcher"
-)
-if not defined BIN if exist "%APPDATA%\Code\User\workspaceStorage\fae98f21f2fe38732848cb82e502ebb5\redhat.java\jdt_ws\the-witcher_71a45e8b\bin\main\java\com\witcher\ui\graphics\GameWindow.class" (
-  set "BIN=%APPDATA%\Code\User\workspaceStorage\fae98f21f2fe38732848cb82e502ebb5\redhat.java\jdt_ws\the-witcher_71a45e8b\bin"
-)
+call "%~dp0compile-swing.bat"
+if errorlevel 1 exit /b 1
 
-if not defined BIN (
-  echo Не найден скомпилированный GameWindow.class
-  echo Собери проект: Ctrl+Shift+B в VS Code
+if not exist "%OUT%\main\java\com\witcher\ui\graphics\GameWindow.class" (
+  echo [ОШИБКА] GameWindow.class не найден после сборки
   pause
   exit /b 1
 )
 
-if exist "%RES%" (
-  xcopy /E /I /Y "%RES%\*" "%BIN%\" >nul
-)
-
-echo Запуск из: %BIN%
-"%JAVA_HOME%\bin\java.exe" -Dsun.java2d.uiScale.enabled=false -Dsun.java2d.dpiaware=true -Dsun.java2d.d3d=false -Dsun.java2d.opengl=false -Dawt.useSystemAAFontSettings=off -Dswing.aatext=false -cp "%BIN%" main.java.com.witcher.ui.graphics.GameWindow
+echo.
+echo Запуск из: %OUT%
+"%JAVA_HOME%\bin\java.exe" -Dsun.java2d.uiScale.enabled=false -Dsun.java2d.dpiaware=true -Dsun.java2d.d3d=false -Dsun.java2d.opengl=false -Dawt.useSystemAAFontSettings=off -Dswing.aatext=false -cp "%OUT%" main.java.com.witcher.ui.graphics.GameWindow
 endlocal
