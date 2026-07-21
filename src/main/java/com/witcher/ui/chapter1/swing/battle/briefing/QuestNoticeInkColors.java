100eb6e517f93d0325b33028b7bf2956cb93620e
top-secret666 <chupacabra2.0666@gmail.com>
2026-07-21 14:57:56 +0300

    refactor(briefing): extract QuestNoticeInkColors

---
+++ src/main/java/com/witcher/ui/chapter1/swing/battle/briefing/QuestNoticeInkColors.java
@@ -0,0 +1,14 @@
+package main.java.com.witcher.ui.chapter1.swing.battle.briefing;
+
+import java.awt.Color;
+
+/** Палитра «чернил» на пергаменте заказа. */
+public final class QuestNoticeInkColors {
+
+  public static final Color HEADER = new Color(28, 14, 4);
+  public static final Color BODY = new Color(32, 18, 6);
+  public static final Color REWARD = new Color(40, 22, 8);
+
+  private QuestNoticeInkColors() {
+  }
+}
