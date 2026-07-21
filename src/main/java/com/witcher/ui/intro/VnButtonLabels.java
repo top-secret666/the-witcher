64bfa7a7070bf615f2d34dca091a335a49d773d9
top-secret666 <chupacabra2.0666@gmail.com>
2026-07-21 14:57:54 +0300

    refactor(vn): extract VnButtonLabels constants

---
+++ src/main/java/com/witcher/ui/intro/VnButtonLabels.java
@@ -0,0 +1,12 @@
+package main.java.com.witcher.ui.intro;
+
+/** Подписи VN-кнопок (Назад, История, Авто). */
+public final class VnButtonLabels {
+
+  public static final String BACK = "Назад";
+  public static final String HISTORY = "История";
+  public static final String AUTO = "Авто";
+
+  private VnButtonLabels() {
+  }
+}
