b1f2ffad0057dfa41b8d9e207f424800fe80b378
top-secret666 <chupacabra2.0666@gmail.com>
2026-07-21 14:57:52 +0300

    refactor(vn): extract shared BossVnTypingConstants

---
+++ src/main/java/com/witcher/chapter1/battle/BossVnTypingConstants.java
@@ -0,0 +1,13 @@
+package main.java.com.witcher.chapter1.battle;
+
+/** Общие тайминги typewriter-VN (брифинг, лес, интро). */
+public final class BossVnTypingConstants {
+
+  public static final int MS_PER_TICK = 16;
+  public static final int TICKS_PER_CHAR = 2;
+  public static final int AUTO_DELAY_TICKS = 50;
+  public static final int AUTO_TICKS_PER_CHAR = 1;
+
+  private BossVnTypingConstants() {
+  }
+}
