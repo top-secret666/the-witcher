92bbfb359a81f0b384457348a5e9313e53b3b730
top-secret666 <chupacabra2.0666@gmail.com>
2026-07-22 17:51:18 +0300

    refactor(ui): add shared MenuCursorPaths constant

---
+++ src/main/java/com/witcher/ui/graphics/MenuCursorPaths.java
@@ -0,0 +1,10 @@
+package main.java.com.witcher.ui.graphics;
+
+/** Пути к общим UI-курсорам. */
+public final class MenuCursorPaths {
+
+  public static final String MENU_CURSOR = "/assets/sprites/menu/menu_cursor.png";
+
+  private MenuCursorPaths() {
+  }
+}
