29627f4a3e0613a019ce8b1e421b7eeb6eff38d2
top-secret666 <chupacabra2.0666@gmail.com>
2026-07-22 17:51:13 +0300

    refactor(ui): remove unused ConsoleUI field and dead code

--- src/main/java/com/witcher/ui/ConsoleUI.java
+++ src/main/java/com/witcher/ui/ConsoleUI.java
@@ -9,18 +9,14 @@ import main.java.com.witcher.repository.ArmourRepository;
 
 import java.util.Scanner;
 
-import static java.awt.SystemColor.text;
-
 
 public class ConsoleUI {
     private final DialogManager dialogManager;
     private final MenuHandler menuHandler;
-    private final int geraltsWallet;
 
     public ConsoleUI(ArmorService armorService, ArmorCalculationService armorCalculationService, ArmorSortingService armorSortingService, SetService setService, InputValidator validator, ArmourRepository armourRepository, int initialWallet) {
         this.dialogManager = new DialogManager(setService, armorCalculationService);
         this.menuHandler = new MenuHandler(armorService, armorCalculationService, armorSortingService, setService, validator, dialogManager, armourRepository, initialWallet);
-        this.geraltsWallet = initialWallet;
     }
 
     public void start() {
@@ -43,21 +39,6 @@ public class ConsoleUI {
             menuHandler.handleMenuChoice(choice);
         }
     }
-
-//    private void showProgressBar() {
-//        String bar = "";
-//        for (int i = 0; i <= 20; i++) {
-//            bar += "█";
-//            System.out.print("\rЗагрузка: [" + bar + "] " + i * 5 + "%");
-//            try {
-//                Thread.sleep(100);
-//            } catch (InterruptedException e) {
-//                Thread.currentThread().interrupt();
-//            }
-//        }
-//        System.out.println();
-//    }
-
 
 private void showShopEntrance() {
     String[] frames = {
