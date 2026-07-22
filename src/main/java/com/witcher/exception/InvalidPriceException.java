bd202e04865c0704078c821679fd721556baf2b9
top-secret666 <chupacabra2.0666@gmail.com>
2026-07-22 17:51:14 +0300

    refactor(exception): align InvalidPriceException with validator bounds

--- src/main/java/com/witcher/exception/InvalidPriceException.java
+++ src/main/java/com/witcher/exception/InvalidPriceException.java
@@ -1,11 +1,11 @@
 package main.java.com.witcher.exception;
 
+import main.java.com.witcher.validation.InputValidator;
+
 public class InvalidPriceException extends ArmourException {
     public InvalidPriceException(int price) {
-        super(String.format("\u001B[31m<<Недопустимая цена: %d крон. Цена должна быть положительной и лежать в диапазоне от 10 до 100000.>>\u001B[0m", price));
+        super(String.format(
+                "\u001B[31m<<Недопустимая цена: %d крон. Цена должна быть положительной и лежать в диапазоне от %d до %d.>>\u001B[0m",
+                price, InputValidator.MIN_PRICE, InputValidator.MAX_PRICE));
     }
-
-//    public InvalidPriceException(String message) {
-//        super("\u001B[31m" + message + "\u001B[0m");
-//    }
 }
