ba323b01b1d4032962d10f36bdbecc2d0bcaf3c5
top-secret666 <chupacabra2.0666@gmail.com>
2026-07-22 17:51:14 +0300

    refactor(repo): clean commented methods from ArmourRepository

--- src/main/java/com/witcher/repository/ArmourRepository.java
+++ src/main/java/com/witcher/repository/ArmourRepository.java
@@ -7,44 +7,13 @@ import java.util.ArrayList;
 public class ArmourRepository {
     private final List<Armour> inventory = new ArrayList<>();
 
-//    public void save(Armour armour) {
-//        inventory.add(armour);
-//    }
-
     public List<Armour> findAll() {
         return new ArrayList<>(inventory);
     }
 
-//    public Optional<Armour> findByName(String name) {
-//        return inventory.stream()
-//                .filter(armour -> armour.getName().equals(name))
-//                .findFirst();
-//    }
-//
-//    public void delete(Armour armour) {
-//        inventory.remove(armour);
-//    }
-//
-//    public void deleteByName(String name) {
-//        inventory.removeIf(armour -> armour.getName().equals(name));
-//    }
-//
-////    public void clear() {
-////        inventory.clear();
-////    }
-
     public int getSize() {
         return inventory.size();
     }
-
-//    public boolean exists(String name) {
-//        return inventory.stream()
-//                .anyMatch(armour -> armour.getName().equals(name));
-//    }
-//
-//    public List<Armour> getInventory() {
-//        return new ArrayList<>(inventory);
-//    }
 
     public void add(Armour armor) {
         inventory.add(armor);
@@ -54,6 +23,3 @@ public class ArmourRepository {
         return findAll();
     }
 }
-
-
-
