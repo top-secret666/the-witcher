3772787918164b0b0dfdabc4ece1750416964554
top-secret666 <chupacabra2.0666@gmail.com>
2026-07-22 17:51:13 +0300

    refactor(service): drop commented dead code from ArmorManagementService

--- src/main/java/com/witcher/service/ArmorManagementService.java
+++ src/main/java/com/witcher/service/ArmorManagementService.java
@@ -1,8 +1,6 @@
 package main.java.com.witcher.service;
 
 import main.java.com.witcher.model.armour.Armour;
-//import main.java.com.witcher.model.sets.ArmourSet;
-//import main.java.com.witcher.model.sets.SchoolSet;
 import main.java.com.witcher.repository.ArmourRepository;
 import main.java.com.witcher.validation.InputValidator;
 
@@ -16,39 +14,10 @@ public class ArmorManagementService extends ArmorService {
         this.armorRepository = armorRepository;
         this.validator = validator;
     }
-//    private boolean canAddArmor(List<Armour> currentSet, Armour newArmor) {
-//        return currentSet.stream()
-//                .noneMatch(armor -> armor.getClass().equals(newArmor.getClass()));
-//    }
 
     public void addArmor(Armour armor) {
         validator.validateName(armor.getName());
         validator.validateWeight(armor.getWeight());
         armorRepository.add(armor);
     }
-
-//    public List<Armour> getArmorByType(ArmourType type) {
-//        return armorInventory.stream()
-//                .filter(armor -> armor.getType() == type)
-//                .collect(Collectors.toList());
-//    }
-//
-//    public List<Armour> getArmorByCategory(ArmourCategory category) {
-//        return armorInventory.stream()
-//                .filter(armor -> armor.getCategory() == category)
-//                .collect(Collectors.toList());
-//    }
-
-//    public List<Armour> getCurrentInventory() {
-//        return new ArrayList<>(armorInventory);
-//    }
-
-//    public boolean isArmorCompatibleWithSet(Armour armor, ArmourSet set) {
-//        if (set instanceof SchoolSet schoolSet) {
-//            return armor.getType() == schoolSet.getSchoolType().getPreferredArmorType();
-//        }
-//
-//        return set.getArmorPieces().stream()
-//                .anyMatch(setPiece -> setPiece.getCategory() == armor.getCategory());
-//    }
 }
