0650af7a3d84b36f6662fd3797b09644fbfe331c
top-secret666 <chupacabra2.0666@gmail.com>
2026-07-22 17:51:14 +0300

    refactor(exception): remove unused ArmourException constructor

--- src/main/java/com/witcher/exception/ArmourException.java
+++ src/main/java/com/witcher/exception/ArmourException.java
@@ -4,8 +4,4 @@ public class ArmourException extends RuntimeException {
     public ArmourException(String message) {
         super("(Ошибка брони:)" + message);
     }
-
-//    public ArmourException(String message, Throwable cause) {
-//        super("(Ошибка брони:)" + message, cause);
-//    }
 }
